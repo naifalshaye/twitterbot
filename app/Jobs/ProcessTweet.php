c733e9a7a243ddeef959ebc7fb858728d225dc19
Naif Al-Shaia <N.AlShaia@tamkeentech.sa>
2017-05-20 16:34:55 +0300

    f

--- app/Jobs/ProcessTweet.php
+++ app/Jobs/ProcessTweet.php
@@ -38,7 +38,6 @@ class ProcessTweet implements ShouldQueue
         $tweet_text = isset($tweet['text']) ? $tweet['text'] : null;
         $user_id = isset($tweet['user']['id_str']) ? $tweet['user']['id_str'] : null;
         $user_screen_name = isset($tweet['user']['screen_name']) ? $tweet['user']['screen_name'] : null;
-        $user_avatar_url = isset($tweet['user']['profile_image_url_https']) ? $tweet['user']['profile_image_url_https'] : null;
 
         if (isset($tweet['id'])) {
             Tweet::create([
@@ -47,7 +46,6 @@ class ProcessTweet implements ShouldQueue
                 'tweet_text' => $tweet_text,
                 'user_id' => $user_id,
                 'user_screen_name' => $user_screen_name,
-                'user_avatar_url' => $user_avatar_url,
                 'approved' => 0
            ]);
         }
