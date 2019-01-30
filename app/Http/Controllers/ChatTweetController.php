a06f7004e3c00e5430eb9ac04a4a51892e67e619
Naif Alshaye <naif@naif.io>
2019-01-30 17:08:58 +0300

    filter chat tweets

--- app/Http/Controllers/ChatTweetController.php
+++ app/Http/Controllers/ChatTweetController.php
@@ -2,15 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Chat;
 use App\ChatTweet;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ChatTweetController extends Controller
 {
     public function index()
     {
-        $tweets = ChatTweet::OrderBy('id','desc')->paginate(15);
-        return view('chat.tweets',compact('tweets'));
+        $chat = ChatTweet::query();
+
+        if (Input::get('keyword')){
+            $chat->where('keyword','like','%'.Input::get('keyword').'%');
+        }
+
+        if (Input::get('user_screen_name')){
+            $chat->where('user_screen_name','like','%'.Input::get('user_screen_name').'%');
+        }
+
+        if (Input::get('user_name')){
+            $chat->where('user_name','like','%'.Input::get('user_name').'%');
+        }
+
+
+        if (Input::get('tweet_text')){
+            $chat->where('tweet_text','like','%'.Input::get('tweet_text').'%');
+        }
+
+        if (Input::get('reply')){
+            $chat->where('reply','like','%'.Input::get('reply').'%');
+        }
+
+        if (Input::get('date')){
+            $chat->where('created_at','like','%'.Input::get('date').'%');
+        }
+
+        $tweets = $chat->OrderBy('id','desc')->paginate(50);
+        $keywords = Chat::get();
+
+        return view('chat.tweets',compact('tweets','keywords'));
     }
 
 }
--- resources/views/chat/tweets.blade.php
+++ resources/views/chat/tweets.blade.php
@@ -21,12 +21,44 @@
                         {{session('success')}}
                     </div>
                 @endif
+
                 <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <form class="form-inline" enctype="multipart/form-data" role="form" method="GET" action="{{'/chat_tweets'}}">
+                                <div class="form-group">
+                                    <input type="date" class="form-control" name="date" placeholder="Date">
+                                </div>
+                                <div class="form-group">
+                                    <select class="form-control" name="keyword" style="width:200px;">
+                                        <option value="">Select keyword</option>
+                                        @foreach ($keywords as $keyword)
+                                            <option value="{{$keyword->keyword}}">{{$keyword->keyword}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="user_screen_name" placeholder="Username">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="user_name" placeholder="Name">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="tweet_text" placeholder="Tweet Text">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="reply" placeholder="Reply Text">
+                                </div>
+                                <button type="submit" class="btn btn-default">Search</button>
+                            </form>
+                        </div>
+                    </div><br>
                     <div class="row">
                         <div class="col-lg-12 table-responsive">
                             <table id="example" cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered hover">
                                 <thead>
                                 <tr>
+                                    <th style="text-align: center; width:180px;">Date</th>
                                     <th style="text-align: center; width:180px;">keyword</th>
                                     <th style="text-align: center; width:170px;">User name</th>
                                     <th style="text-align: center; width:170px;">Name</th>
@@ -37,6 +69,7 @@
                                 <tbody>
                                 @foreach($tweets as $tweet)
                                     <tr>
+                                        <td style="text-align: center;">{{ \Carbon\Carbon::createFromTimeString($tweet->created_at)->toDateString() }}</td>
                                         <td style="text-align: center;">{{ $tweet->keyword }}</td>
                                         <td style="text-align: center;"><a href="http://twitter.com/{{ $tweet->user_screen_name }}" target="_blank">{{ $tweet->user_screen_name }}</a></td>
                                         <td style="text-align: center;">{{ $tweet->user_name }}</td>
