0f866718b0a1669727678ae248a045080b7e1013
Naif <naif@naif.io>
2019-02-02 00:52:25 +0300

    fix

--- app/Console/Commands/StreamTwitter.php
+++ app/Console/Commands/StreamTwitter.php
@@ -41,7 +41,7 @@ class StreamTwitter extends Command
         if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
             error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
         }
-        
+
         $conf = Conf::findOrNew(1);
         if ($conf->turn_off){
             return;
