ac47645a82e129f2a6f4b66f767f6daf34ae0188
Naif <naif@naif.io>
2019-02-02 16:26:57 +0300

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends Controller
             ->buildOauth($url, $requestMethod)
             ->performRequest());
 
-        if (isset($trends->errors)){
+        if (isset($trends->errors) && !empty($trends->errors)){
             $trends = null;
         }
         $chat_tweet = ChatTweet::get()->last();
