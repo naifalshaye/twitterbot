8fe7dfc91aedf7bd0244265b03c90d25441981c3
Naif <naif@naif.io>
2019-02-09 00:03:07 +0300

    fix

--- resources/views/archive/index.blade.php
+++ resources/views/archive/index.blade.php
@@ -24,7 +24,7 @@
                     </div>
                 @endif
                 <div class="panel-body">
-                    <div align="center" class="alert alert-danger">Add keywords that you want to search and archive.<br>Maximum 10 keywords and max length is 500 characters for all keywords together.<br>Queries can be limited due to complexity</div>
+                    <div align="center" class="alert alert-warning">Add keywords that you want to search and archive.<br>Maximum 10 keywords and max length is 500 characters for all keywords together.<br>Queries can be limited due to complexity</div>
 
                     <div class="row">
 
